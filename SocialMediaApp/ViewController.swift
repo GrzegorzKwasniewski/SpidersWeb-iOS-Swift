b8c31807bafb2248824744e82bc4c342ba8b2718
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2016-09-05 11:24:17 +0200

    Initial Commit

---
+++ SocialMediaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz Kwaśniewski on 05/09/16.
+//  Copyright © 2016 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
