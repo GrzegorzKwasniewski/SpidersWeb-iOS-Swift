920f206d21236289f59c4ff5c915cceda6d00164
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2016-10-12 15:22:22 +0200

    Make new UI for FeedVC

---
+++ SocialMediaApp/RoundedContentView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedContentView.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz Kwaśniewski on 12/10/16.
+//  Copyright © 2016 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import UIKit
+
+class RoundedContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SocialMediaApp/TapImageView.swift
@@ -0,0 +1,21 @@
+//
+//  TapImageView.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz Kwaśniewski on 11/10/16.
+//  Copyright © 2016 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import UIKit
+
+class TapImageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
