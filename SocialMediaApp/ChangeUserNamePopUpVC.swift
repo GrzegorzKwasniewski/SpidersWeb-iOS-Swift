7b007bb5f0e055d2efaf941dc1d06547444036c6
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2016-11-05 12:20:51 +0100

    Make animation for change user name Pop Up

--- SocialMediaApp/ChangeUserNamePopUpVC.swift
+++ SocialMediaApp/ChangeUserNamePopUpVC.swift
@@ -15,13 +15,27 @@ class ChangeUserNamePopUpVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        showAnimate()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func showAnimate() {
+        self.view.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+    }
+    
+    func removeAnimate() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+            }, completion:{(finished : Bool)  in
+                if (finished) {
+                    self.view.removeFromSuperview()
+                }
+        })
     }
     
     @IBAction func changeUserName(_ sender: AnyObject) {
@@ -44,15 +58,9 @@ class ChangeUserNamePopUpVC: UIViewController {
             }
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func removePopUp(_ sender: AnyObject) {
+        removeAnimate()
     }
-    */
 
 }
