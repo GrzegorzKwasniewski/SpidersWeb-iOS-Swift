c7d9f11072f71bebe37d8afe00ed7ec41a8b91b5
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-01-04 13:11:47 +0100

    Make sign up / login screen

--- SocialMediaApp/ShadowButton.swift
+++ SocialMediaApp/ShadowButton.swift
@@ -14,11 +14,13 @@ class ShadowButton: UIButton {
         super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-        layer.opacity = 0.8
+        layer.opacity = 1.0
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         
-        layer.cornerRadius = 2.0
+        layer.cornerRadius = 30.0
     }
 
 }
