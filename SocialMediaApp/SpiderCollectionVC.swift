13c7ecac8d5e6f548f789827cf27688d74ae9c73
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-01-04 22:09:29 +0100

    Add code for handling UICollectionView and UISearchBar in SpiderCollectionVC

--- SocialMediaApp/SpiderCollectionVC.swift
+++ SocialMediaApp/SpiderCollectionVC.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class SpiderCollectionVC: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        searchBar.delegate = self
+        
+        searchBar.returnKeyType = UIReturnKeyType.done
+
     }
 
     @IBAction func goToUserSettings(_ sender: AnyObject) {
@@ -22,5 +30,43 @@ class SpiderCollectionVC: UIViewController {
         } else {
             print("README: There's no slideMenuController - check if You have container ViewController for slide menu")
         }
+    }
+}
+
+extension SpiderCollectionVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "spiderCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    /* func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 105, height: 105)
+    } */
+
+}
+
+extension SpiderCollectionVC: UISearchBarDelegate {
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
     }
 }
