2e8ff7c0f38b3d6abf389954fe98fd9399774fb9
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-08-21 22:41:37 +0200

    Add Custom Struct For Colors Values

---
+++ SocialMediaApp/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz on 21/08/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    
+    static let MAIN_COLOR = UIColor(red: 0.0, green: 170.0 / 255.0, blue: 141.0 / 255.0, alpha: 1)
+    
+    static let SECOND_COLOR = UIColor(red: 105.0 / 255.0, green: 26.0 / 255.0, blue: 153.0 / 255.0, alpha: 1)
+    
+    static let ACENT_COLOR = UIColor(red: 104.0 / 255.0, green: 239.0 / 255.0, blue: 173.0 / 255.0, alpha: 1)
+    
+    static let GREY_TEXT_COLOR = UIColor(red: 179.0 / 255.0, green: 179.0 / 255.0, blue: 179.0 / 255.0, alpha: 1)
+    
+    static let GREY_SHADOW_COLOR = UIColor(red: 179.0 / 255.0, green: 179.0 / 255.0, blue: 179.0 / 255.0, alpha: 1)
+    
+}
