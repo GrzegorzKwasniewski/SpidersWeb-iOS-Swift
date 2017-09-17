533079c983b7c0c85a798abf8ef70a2c550acbc6
GrzegorzKwasniewski <contact@grzegorzkwasniewski.com>
2017-09-17 14:09:28 +0200

    Create Struct For Storing Strings

---
+++ SocialMediaApp/StringsConstants.swift
@@ -0,0 +1,19 @@
+//
+//  StringConstants.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz on 17/09/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import Foundation
+
+struct UserMessages {
+
+    public static var userName = "New user"
+
+}
+
+struct URLS {
+    public static var defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/socialmediaapp-fcfde.appspot.com/o/default-avatars%2Fdefault-avatar.png?alt=media&token=56651f7e-5959-428e-93d7-b3059ffc77fb"
+}
