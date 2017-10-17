341b59e261bd591e4365762011d20e5c703724d2
GrzegorzKwasniewski <contact@grzegorzkwasniewski.com>
2017-10-17 14:33:45 +0200

    Add Custom Text Label

---
+++ SocialMediaApp/WhiteTextLabel.swift
@@ -0,0 +1,34 @@
+//
+//  WhiteTextLabel.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz on 17/10/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import UIKit
+
+class WhiteTextLabel: UILabel {
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+        textColor = .white
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.5
+        
+        backgroundColor = UIColor.clear
+        layer.masksToBounds = false
+        
+        font = UIFont(name: "Avenir-Heavy", size: 18.0)
+        textAlignment = .left
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+}
