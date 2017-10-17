d9bb773c9365f3e11f797cfaef6c5f3edc80acb5
GrzegorzKwasniewski <contact@grzegorzkwasniewski.com>
2017-10-17 14:46:09 +0200

    Unit Test - Add First Test

---
+++ SocialMediaAppTests/AuthenticationFailedPopUpTest.swift
@@ -0,0 +1,44 @@
+//
+//  FirstTest.swift
+//  SocialMediaApp
+//
+//  Created by Grzegorz on 16/10/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import SocialMediaApp
+
+class AuthenticationFailedPopUpTest: QuickSpec {
+    
+    override func spec() {
+        
+        describe("AuthenticationFailedPopUp view") {
+            
+            context("After being properly initialized") {
+                
+                let popUp: AuthenticationFailedPopUp = AuthenticationFailedPopUp()
+                
+                it("Should have blur view added") {
+                
+                    expect(popUp.blurView).toNot(beNil())
+                    
+                }
+                
+                it("should have dissmis button added") {
+                
+                    let dismissButton = popUp.dismissButton
+
+                    let expectedAccessibilityLabel = "dismissButton"
+                    
+                    expect(dismissButton?.accessibilityLabel).to(equal(expectedAccessibilityLabel))
+                    
+                }
+            }
+        }
+    }
+}
+
