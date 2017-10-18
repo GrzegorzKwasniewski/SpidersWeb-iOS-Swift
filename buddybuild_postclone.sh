0050edb10ae726b1b1ae3e659601e1ba923436a7
Grzegorz Kwaśniewski <contact@grzegorzkwasniewski.com>
2017-10-18 09:44:03 +0200

    Update buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,9 +1,1 @@
-# Install swiftlint if necessary
-if ! which swiftlint >/dev/null; then
-    brew install swiftlint
-fi
 
-# Run Swiftlint
-echo "Here comes the output of Swiftlint"
-cd project_folder
-swiftlint
