985b5e318ab0c0bec14d7fb6a025ad526b192d7c
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2017-10-19 20:45:11 +0200

    Refactor Code In AppDelegate

--- SocialMediaApp/AppDelegate.swift
+++ SocialMediaApp/AppDelegate.swift
@@ -14,6 +14,8 @@ import TwitterKit
 import Fabric
 import UserNotifications
 
+// MARK: Schema Dependent Variables
+
 #if DEVELOPMENT
 let SERVER_URL = "dev"
 let API_TOKEN = ""
@@ -24,8 +26,12 @@ let API_TOKEN = ""
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: Fields
 
     var window: UIWindow?
+    
+    // MARK: Aplication State Functions
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
@@ -73,32 +79,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             Twitter.sharedInstance().start(withConsumerKey: key, consumerSecret: secret)
         }
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-//    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-//        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?, annotation: nil)
-//    }
     
     func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
         -> Bool {
@@ -107,23 +87,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 return true
             }
             
-            return self.application(application,
-                                    open: url,
-                                    sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?,
-                                    annotation: [:])
+            return self.application(
+                application,
+                open: url,
+                sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?,
+                annotation: [:])
     }
     
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        if GIDSignIn.sharedInstance().handle(url,
-                                             sourceApplication: sourceApplication,
-                                             annotation: annotation) {
+        if GIDSignIn.sharedInstance().handle(
+            url,
+            sourceApplication: sourceApplication,
+            annotation: annotation) {
+            
             return true
         }
-        return FBSDKApplicationDelegate.sharedInstance().application(application,
-                                                                     open: url,
-                                                                     sourceApplication: sourceApplication,
-                                                                     annotation: annotation)
+        
+        return FBSDKApplicationDelegate.sharedInstance().application( application,
+            open: url,
+            sourceApplication: sourceApplication,
+            annotation: annotation)
     }
-
 }
 
