dc48f3eec028450681a3a9ac6ab9c4aa69788247
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-10-22 22:30:41 +0200

    Update Spider Model

--- SocialMediaApp/Spider.swift
+++ SocialMediaApp/Spider.swift
@@ -9,130 +9,64 @@
 import Foundation
 import FirebaseDatabase
 
-class Spider {
-
-    private var _spiderUid: String = ""
-    private var _spiderId: String = ""
-    private var _name: String = ""
-    private var _genus: String = ""
-    private var _species: String = ""
-    private var _commonName: String = ""
-    private var _countryOrigin: String = ""
-    private var _group: String = ""
-    private var _recivedFrom: String = ""
+public struct Spider {
     
-    private var _dateRecived: Date = Date()
-    private var _imageUrl: String = ""
-    
-    /* IMPLEMENT AT LATER STAGE
-    var last_fed
-    var days_houngry
-    var feed_every_days
-    var feed_days_after_molt
-    var sex
-    var type
-    var venom
-    var temperament
-    var longevity
-    var u_bristels
-    var temperature_low
-    var temperature_heigh
-    var humidity
-    var last_rehouse
-    var substrate_changed
-    var status
-    var pre_molt
-    var life_stages
-    var notes */
-    
-    var spiderUid: String {
-        return _spiderUid
-    }
-    
-    var spiderId: String {
-        return _spiderId
-    }
-    
-    var name: String {
-        return _name
-    }
-    
-    var genus: String {
-        return _genus
-    }
-    
-    var species: String {
-        return _species
-    }
-    
-    var commonName: String {
-        return _commonName
-    }
-    
-    var countryOrigin: String {
-        return _countryOrigin
-    }
-    
-    var group: String {
-        return _group
-    }
-    
-    var recivedFrom: String {
-        return _recivedFrom
-    }
-    
-    var dateRecived: Date {
-        return _dateRecived
-    }
-    
-    var imageUrl: String {
-        return _imageUrl
-    }
+    private(set) var spiderUid: String = ""
+    private(set) var spiderId: String = ""
+    private(set) var name: String = ""
+    private(set) var genus: String = ""
+    private(set) var species: String = ""
+    private(set) var commonName: String = ""
+    private(set) var countryOrigin: String = ""
+    private(set) var group: String = ""
+    private(set) var recivedFrom: String = ""
+    private(set) var dateRecived: Date = Date()
+    private(set) var imageUrl: String = ""
     
     init() {}
     
     init(spiderId: String, spiderData: Dictionary<String, AnyObject>) {
         
-        _spiderId = spiderId
+        self.spiderId = spiderId
         
         if let spiderUid = spiderData["spiderUid"] as? String {
-            _spiderUid = spiderUid
+            self.spiderUid = spiderUid
         }
         
         if let name = spiderData["name"] as? String {
-            _name = name
+            self.name = name
         }
         
         if let genus = spiderData["genus"] as? String {
-            _genus = genus
+            self.genus = genus
         }
         
         if let species = spiderData["species"] as? String {
-            _species = species
+            self.species = species
         }
         
         if let commonName = spiderData["commonName"] as? String {
-            _commonName = commonName
+            self.commonName = commonName
         }
         
         if let countryOrigin = spiderData["countryOrigin"] as? String {
-            _countryOrigin = countryOrigin
+            self.countryOrigin = countryOrigin
         }
-
+        
         if let group = spiderData["group"] as? String {
-            _group = group
+            self.group = group
         }
         
         if let recivedFrom = spiderData["recivedFrom"] as? String {
-            _recivedFrom = recivedFrom
+            self.recivedFrom = recivedFrom
         }
         
         if let dateRecived = spiderData["dateRecived"] as? Date {
-            _dateRecived = dateRecived
+            self.dateRecived = dateRecived
         }
         
         if let imageUrl = spiderData["imageUrl"] as? String {
-            _imageUrl = imageUrl
+            self.imageUrl = imageUrl
         }
     }
 }
