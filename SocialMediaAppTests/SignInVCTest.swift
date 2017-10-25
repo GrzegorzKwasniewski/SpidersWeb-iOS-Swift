898038dfce5c919fd0a1837919c7f892cda27fe4
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2017-10-25 18:59:32 +0200

    Unit Test - Test Image Cache Creation

---
+++ SocialMediaAppTests/SignInVCTest.swift
@@ -0,0 +1,37 @@
+//
+//  SignInVCTest.swift
+//  SocialMediaApp
+//
+//  Created by serwis on 25/10/2017.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import SocialMediaApp
+
+class SignInVCTest: QuickSpec {
+
+    override func spec() {
+        
+        var signInVC: SignInVC!
+        
+        describe("SignInVC") {
+            
+            beforeEach {
+                signInVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SignInVC") as! SignInVC
+
+                _ = signInVC.view
+            }
+            
+            describe("did load") {
+                
+                it("should have NSCache assigned") {
+                    expect(SignInVC.imageCache).notTo(beNil())
+                }
+            }
+        }
+    }
+}
