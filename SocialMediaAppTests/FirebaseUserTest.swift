cd4b18c73227a497932ad28d10c39a65e7ab0bb4
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2017-10-25 14:22:02 +0200

    Unit Test - Test User Creation With Proper Data

--- SocialMediaAppTests/FirebaseUserTest.swift
+++ SocialMediaAppTests/FirebaseUserTest.swift
@@ -20,19 +20,34 @@ class FirebaseUserTest: QuickSpec {
             
             var fireBaseUser: FirebaseUser!
             
-            beforeEach {
-                
-            }
-            
             context("when default initializer is used") {
                 
-                fireBaseUser = FirebaseUser()
-                
                 it("should set all properties to default value") {
+                    
+                    fireBaseUser = FirebaseUser()
+
                     expect(fireBaseUser.uid).to(equal("no data"))
                     expect(fireBaseUser.displayName).to(equal("no data"))
                     expect(fireBaseUser.email).to(equal("no data"))
                     expect(fireBaseUser.image).to(equal(#imageLiteral(resourceName: "default-avatar")))
+                }
+            }
+            
+            context("when valid data is passed") {
+                
+                it("should set correctly all properties") {
+                    
+                    let userUID = UUID().uuidString
+                    let displayName = "Marek"
+                    let email = "marek@gmail.com"
+                    let image = #imageLiteral(resourceName: "purple-cartoon-spider")
+                    
+                    fireBaseUser = FirebaseUser(userUid: userUID, userDisplayName: displayName, userEmail: email, userImage: image)
+                    
+                    expect(fireBaseUser.uid).to(equal(userUID))
+                    expect(fireBaseUser.displayName).to(equal(displayName))
+                    expect(fireBaseUser.email).to(equal(email))
+                    expect(fireBaseUser.image).to(equal(image))
                 }
             }
         }
