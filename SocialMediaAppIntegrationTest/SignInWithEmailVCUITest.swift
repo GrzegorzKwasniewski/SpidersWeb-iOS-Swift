77337ba48bf19bdfeac237831c5c0dd01a7a3714
GrzegorzKwasniewski <contact@grzegorzkwasniewski.com>
2017-10-28 13:12:32 +0200

    Test For Buddy Bulid

--- SocialMediaAppIntegrationTest/SignInWithEmailVCUITest.swift
+++ SocialMediaAppIntegrationTest/SignInWithEmailVCUITest.swift
@@ -32,7 +32,7 @@ class SignInWithEmailVCUITest: BaseUITests {
      Test if warning alert will be shown when user will not provide ANY email login data on login screen
      */
     
-    func testEmptyEmailAndPasswordField_ShowAlert() {
+    func testEmptyEmailAndPasswordField_ShowAlert2() {
         tapButton(buttonName: Name.loginWithEmailButton)
         expectToSeeAlert(text: Name.errorMessage)
         tapButton(buttonName: Name.dismissButton)
@@ -43,7 +43,7 @@ class SignInWithEmailVCUITest: BaseUITests {
      Test if warning alert will be shown when user will provide INVALID    email login data on login screen
      */
     
-    func testInvalidEmailAndPasswordData_ShowAlert() {
+    func testInvalidEmailAndPasswordData_ShowAlert2() {
         provideInvalidEmail()
         provideInvalidPassword()
         tapButton(buttonName: Name.loginWithEmailButton)
@@ -56,7 +56,7 @@ class SignInWithEmailVCUITest: BaseUITests {
      Test if user will be loged in to the app when valid data will be provided
      */
     
-    func testValidEmailAndPasswordData_ShowAlert() {
+    func testValidEmailAndPasswordData_ShowAlert2() {
         provideValidEmail()
         provideValidPassword()
         tapButton(buttonName: Name.loginWithEmailButton)
