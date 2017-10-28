0e6f38d8cde2f7b156780ca7a5810973722c738b
GrzegorzKwasniewski <contact@grzegorzkwasniewski.com>
2017-10-28 15:52:12 +0200

    Add Dummy Test

--- SocialMediaAppTests/CellSpiderTest.swift
+++ SocialMediaAppTests/CellSpiderTest.swift
@@ -18,8 +18,24 @@ class CellSpiderTest: QuickSpec {
         
         describe("Cell Spider") {
             
-            context("was initialized") {
-                
+            context("was configured with image") {
+//                
+//                var spiderCollectionVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SpiderCollectionVC") as! SpiderCollectionVC
+//                
+//                spiderCollectionVC.dataService = DataServiceMock()
+//                
+//                _ = spiderCollectionVC.view
+//                
+//                let spider = DataServiceMock.crateSpiderTestData()
+//                
+//                let cellSpider = spiderCollectionVC.collectionView.cellForItem(at: IndexPath(row: 0, section: 0)) as! CellSpider
+//                
+//                it("should have properties properly set") {
+//                    cellSpider.configureCell(spider: spider, image: #imageLiteral(resourceName: "default-avatar"))
+//                    
+//                    expect(cellSpider.spiderName.text).to(equal("testName"))
+//                    expect(cellSpider.spiderImage.image).to(equal(#imageLiteral(resourceName: "default-avatar")))
+//                }
             }
         }
     }
